3b509c756bbdc8b2161dc85f37053bbeb3c951e9
RohithRajTeddu <teddurohithraj@gmail.com>
2025-06-26 05:10:56 -0500

    Update main.tf

--- terraformday-1/main.tf
+++ terraformday-1/main.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "name" {
-    ami = var.ami_id
-    instance_type = var.ami_id
+    ami = "ami-0b09627181c8d5778"
+    instance_type = "t2.micro"
+tags = {
+    Name = "junikens123-ec2"
+  }
 }
